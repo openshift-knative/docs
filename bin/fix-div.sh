e54fba0e0ee705c1b0a5003d285cbbce0d5c49fb
Joan <joaedwar@localhost.localdomain>
2019-05-07 17:29:31 +0100

    aronta site structure

--- bin/fix-div.sh
+++
@@ -1,4 +0,0 @@
-# fix html to reverse divs output from asciidco block
-
-find ./build -type f -exec \
-    perl -0pi -e 's/<div class="content">\n(<div class="title">.*)/\1<div class="content">/g' {} +
